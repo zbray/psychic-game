5f2efcae6db1e5e10efedd00490fd3140f14fd66
Cold Zebra <zachdbray@gmail.com>
2018-12-09 22:39:40 -0600

    coming together and moving things around

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,38 @@
-//Parameters for keeping score
+//Variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-
-//An array to hold guessed letters
-var guessedLetters = [];
-
-//Alphabet, playing pieces for the user and computer
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var guessesLeft = 9; //Every round begins with 9 chances to win
+var guessedLetters = []; //An array to hold guessed letters
+var alphabet = "abcdefghijklmnopqrstuvwxyz"; //Alphabet, playing pieces for the user and computer
 
 //Make the computer make a choice at random from alphabet and consolog.log its choice
 var randomLetter = Math.floor(Math.random() * alphabet.length);
 var computerChoice = alphabet[randomLetter];
 console.log(computerChoice);
 
-//Running the game program
 
-//Listener to register guesses
-document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keycode).toLowerCase();
-    guessedLetters.push(userGuess);
+function reset(){ // this code showed up twice so I'm saving the space by making it a function
+    guessesLeft = 9; //this resets the guess counter
+    guessedLetters = ""; //this removes the displayed guesses from the previous round
 }
 
+//Game Program
 
+//Listener to register guesses
+document.onkeyup = function(event) { //runs the game on key up
+    var userGuess = String.fromCharCode(event.keycode).toLowerCase(); //storing userGuess as a lower case to compare to computerChoice later
+    guessedLetters.push(userGuess); //displaying to user the letters they guessed
+    guessesLeft--; //everytime the user guesses the counter subtracts 1
 
-
-
-//Game Win/Loss Condition
-
-if (userGuess === computerChoice){
-    document.getElementById("wins").innerHTML = wins++;
-    )
+//Game Win Condition
+    if (userGuess == computerChoice){
+        wins++;
+        alert("You must be psychic! Congratulations on guessing correctly. You've won this round!");
+        reset();
 }
     
 
 if (userGuess === !computerChoice){
     ;
+}
 }
